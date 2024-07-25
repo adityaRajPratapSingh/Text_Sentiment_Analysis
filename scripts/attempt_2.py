641bdaab24f944406283a315ad55da305f2eddc2
adityaRajPratapSingh <magellan2409@gmail.com>
2024-07-25 15:01:05 +0530

    Ready for dynamic learning rate training for 50

--- scripts/attempt_2.py
+++ scripts/attempt_2.py
@@ -13,9 +13,13 @@ class G:
     PADDING='post'
     TRUNCATING='post'
     MAXLEN=120
+    OPTIMIZER=tf.keras.optimizers.Adam()
+    LOSS=tf.keras.losses.sparse_categorical_crossentropy
+    STOP_PATIENCE=20
+    EMB_DIM=64
 
 all_sentences, all_labels = [], []
-with open("\\wsl.localhost\Ubuntu-22.04\home\magellan\envs\tensorflow_cuda_1\data\archive\text.csv") as csvfile:
+with open("/home/magellan/envs/tensorflow_cuda_1/project/data/archive/text.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     next(reader)
 
@@ -42,3 +46,28 @@ padded_sequences=pad_sequences(sequences, maxlen=G.MAXLEN, padding=G.PADDING, tr
 testing_sequences=tokenizer.texts_to_sequences(testing_sentences)
 padded_testing_sequences=padded_sequences(testing_sequences, maxlen=G.MAXLEN, padding=G.PADDING, truncating=G.TRUNCATING)
 
+lr_schedule = tf.keras.callbacks.LerarningRateScheduler(
+    lambda epoch:1e-8 * 10**(epoch /20)
+)
+
+early_stop=tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=G.STOP_PATIENCE)
+
+model=tf.keras.Sequential([
+    tf.keras.layers.Embedding(G.NUM_WORDS, G.EMB_DIM, input_length=G.MAXLEN),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(2048, activation='relu'),
+    tf.keras.layers.Dense(1024, activation='relu'),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(6, activation='softmax')
+])
+
+model.compile(
+    loss=G.LOSS,
+    optimizer=G.OPTIMIZER,
+    metrics=['accuracy']
+)
+
+model.fit(padded_sequences, training_labels, epochs=G.EPOCHS, validation_data=(padded_testing_sequences, testing_labels), callbacks=[lr_schedule, early_stop])
