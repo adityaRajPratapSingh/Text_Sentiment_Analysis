459d5de0c7593d3c65536d32db49106664958dc5
adityaRajPratapSingh <magellan2409@gmail.com>
2024-07-25 13:42:04 +0530

    Second Commit: padding sequences training and testing

--- scripts/attempt_2.py
+++ scripts/attempt_2.py
@@ -2,10 +2,17 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 class G:
     EPOCHS = 50
     DATA_SPLIT=0.9
+    NUM_WORDS=100000
+    OOV_TOKEN='<OOV>'
+    PADDING='post'
+    TRUNCATING='post'
+    MAXLEN=120
 
 all_sentences, all_labels = [], []
 with open("\\wsl.localhost\Ubuntu-22.04\home\magellan\envs\tensorflow_cuda_1\data\archive\text.csv") as csvfile:
@@ -18,6 +25,7 @@ with open("\\wsl.localhost\Ubuntu-22.04\home\magellan\envs\tensorflow_cuda_1\dat
 
 total_length=len(all_sentences)
 split = int(total_length*G.DATA_SPLIT)
+
 training_sentences=all_sentences[:split]
 training_labels=all_labels[:split]
 testing_sentences=all_sentences[split:]
@@ -25,4 +33,12 @@ testing_labels=all_labels[split:]
 
 training_labels=np.array(training_labels)
 testing_labels=np.array(testing_labels)
+
+tokenizer=Tokenizer(oov_token=G.OOV_TOKEN, num_words=G.NUM_WORDS)
+tokenizer.fit_on_texts(training_sentences)
+sequences=tokenizer.texts_to_sequences(training_sentences)
+padded_sequences=pad_sequences(sequences, maxlen=G.MAXLEN, padding=G.PADDING, truncating=G.TRUNCATING)
+
+testing_sequences=tokenizer.texts_to_sequences(testing_sentences)
+padded_testing_sequences=padded_sequences(testing_sequences, maxlen=G.MAXLEN, padding=G.PADDING, truncating=G.TRUNCATING)
 
